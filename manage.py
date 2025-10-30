db586fb8d08d4cd728f494278a085ad1cea61e5c
awaw92 <adrianwawrzyniak92@wp.pl>
2025-10-30 20:02:24 +0100

    Restructured repo: moved contents out of wiki folder and removed pycache

--- manage.py
+++ manage.py
@@ -6,6 +6,9 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wiki.settings')
+
+
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
